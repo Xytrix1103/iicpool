5156e070bddde22e2f139ce9ed2600b8704958ae
Ho Shu Yi <syho031001@gmail.com>
2024-08-25 21:47:09 +0800

    migrate home to custom layout

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,114 +1,101 @@
-import React, { useEffect, useState } from 'react'
-import { Pressable, ScrollView, View } from 'react-native'
-import FirebaseApp from '../components/FirebaseApp'
-import { collection, onSnapshot } from 'firebase/firestore'
+import React from 'react'
+import { Pressable, StyleSheet, View } from 'react-native'
 import { Appbar, Button } from 'react-native-paper'
 import { logout } from '../api/auth'
 import CustomText from '../components/themed/CustomText'
 import { useNavigation } from '@react-navigation/native'
 import Icon from '@expo/vector-icons/MaterialCommunityIcons'
+import CustomLayout from '../components/themed/CustomLayout'
+import CustomFlex from '../components/themed/CustomFlex'
+
+const CustomAppbarItem = ({ icon, text, onPress }: { icon: string, text: string, onPress: () => void }) => {
+	return (
+		<Pressable style={style.button} onPress={onPress}>
+			<Icon
+				// @ts-ignore
+				name={icon} size={24} color="grey"
+			/>
+			<CustomText size={12} bold={true} color="grey">{text}</CustomText>
+		</Pressable>
+	)
+}
 
 const Home = () => {
-	const [test, setTest] = useState<any[]>([])
 	const navigation = useNavigation()
-	const { db } = FirebaseApp
 	
-	useEffect(() => {
-		//display all documents in "test" collection
-		const unsubscribe = onSnapshot(collection(db, 'test'), snapshot => {
-			const data: any[] = []
-			snapshot.forEach(doc => {
-				console.log(doc.data())
-				data.push(doc.data())
-			})
-			setTest(data)
-		})
-		
-		return () => {
-			unsubscribe()
-		}
-	}, [db])
+	const footer = (
+		<Appbar style={style.appbar}>
+			<View style={style.buttonsContainer}>
+				<CustomAppbarItem
+					icon="compass"
+					text="Home"
+					onPress={() => {
+						// @ts-ignore
+						navigation.navigate('Home')
+					}}
+				/>
+				<CustomAppbarItem
+					icon="script-text"
+					text="Activity"
+					onPress={() => {
+						// @ts-ignore
+						navigation.navigate('Home')
+					}}
+				/>
+				<CustomAppbarItem
+					icon="message-processing-outline"
+					text="Messages"
+					onPress={() => {
+						// @ts-ignore
+						navigation.navigate('Home')
+					}}
+				/>
+				<CustomAppbarItem
+					icon="account-circle-outline"
+					text="Profile"
+					onPress={() => {
+						// @ts-ignore
+						navigation.navigate('Profile')
+					}}
+				/>
+			</View>
+		</Appbar>
+	)
 	
-	// @ts-ignore
 	return (
-		<View style={{
-			width: '100%',
-			height: '100%',
-		}}>
-			<ScrollView style={{
-				padding: 20,
-				width: '100%',
-				height: '100%',
-			}}>
+		<CustomLayout
+			footer={footer}
+			scrollable={true}
+		>
+			<CustomFlex>
 				<CustomText>Home</CustomText>
 				<Button onPress={() => {
 					// @ts-ignore
 					navigation.navigate('RideToCampus')
 				}}>Ride to Campus</Button>
 				<Button onPress={logout}>Logout</Button>
-				{test.map((item, index) => (
-					<CustomText key={index}>{item.test}</CustomText>
-				))}
-			</ScrollView>
-			<Appbar style={{
-				width: '100%',
-				justifyContent: 'center',
-				alignItems: 'center',
-			}}>
-				<View style={{
-					flexDirection: 'row',
-					flexWrap: 'wrap',
-					width: '100%',
-					justifyContent: 'space-evenly',
-				}}>
-					<Pressable style={{
-						flexDirection: 'column',
-						gap: 5,
-						alignItems: 'center',
-					}} onPress={() => {
-						// @ts-ignore
-						navigation.navigate('Home')
-					}}>
-						<Icon name="compass" size={24} color="darkred" />
-						<CustomText size={12} bold={true} color="darkred">Home</CustomText>
-					</Pressable>
-					<Pressable style={{
-						flexDirection: 'column',
-						gap: 5,
-						alignItems: 'center',
-					}} onPress={() => {
-						// @ts-ignore
-						navigation.navigate('Home')
-					}}>
-						<Icon name="script-text" size={24} color="grey" />
-						<CustomText size={12} bold={true} color="grey">Activity</CustomText>
-					</Pressable>
-					<Pressable style={{
-						flexDirection: 'column',
-						gap: 5,
-						alignItems: 'center',
-					}} onPress={() => {
-						// @ts-ignore
-						navigation.navigate('Home')
-					}}>
-						<Icon name="message-processing-outline" size={24} color="grey" />
-						<CustomText size={12} bold={true} color="grey">Messages</CustomText>
-					</Pressable>
-					<Pressable style={{
-						flexDirection: 'column',
-						gap: 5,
-						alignItems: 'center',
-					}} onPress={() => {
-						// @ts-ignore
-						navigation.navigate('Profile')
-					}}>
-						<Icon name="account-circle-outline" size={24} color="grey" />
-						<CustomText size={12} bold={true} color="grey">Account</CustomText>
-					</Pressable>
-				</View>
-			</Appbar>
-		</View>
+			</CustomFlex>
+		</CustomLayout>
 	)
 }
+
+const style = StyleSheet.create({
+	appbar: {
+		width: '100%',
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+	buttonsContainer: {
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		width: '100%',
+		justifyContent: 'space-evenly',
+	},
+	button: {
+		flexDirection: 'column',
+		gap: 5,
+		alignItems: 'center',
+	},
+})
 
 export default Home
